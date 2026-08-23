71cbef4e4e384e9d9a88b5fa1b20419284134e87
youkorr <96953496+youkorr@users.noreply.github.com>
2026-08-23 04:27:26 +0200

    p4_webview: match usb_display_panel configuration

--- components/p4_webview/__init__.py
+++ components/p4_webview/__init__.py
@@ -8,18 +8,32 @@ p4_webview_ns = cg.esphome_ns.namespace("p4_webview")
 P4WebView = p4_webview_ns.class_("P4WebView", cg.Component)
 
 CONF_URL = "url"
+CONF_HOST = "host"
+CONF_PORT = "port"
+CONF_WIDTH = "width"
+CONF_HEIGHT = "height"
+CONF_ROTATE = "rotate"
+CONF_TOUCH_ROTATE = "touch_rotate"
+CONF_KIOSK = "kiosk"
+CONF_TOKEN = "token"
+CONF_STATS = "stats"
 CONF_DISPLAY_ID = "display_id"
 CONF_TOUCHSCREEN_ID = "touchscreen_id"
-CONF_KIOSK = "kiosk"
-CONF_ROTATION = "rotation"
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(P4WebView),
     cv.Required(CONF_URL): cv.string,
+    cv.Optional(CONF_HOST): cv.string,
+    cv.Optional(CONF_PORT, default=8123): cv.port,
+    cv.Optional(CONF_WIDTH, default=800): cv.positive_int,
+    cv.Optional(CONF_HEIGHT, default=1280): cv.positive_int,
+    cv.Optional(CONF_ROTATE, default=0): cv.one_of(0, 90, 180, 270, int=True),
+    cv.Optional(CONF_TOUCH_ROTATE, default=0): cv.one_of(0, 90, 180, 270, int=True),
+    cv.Optional(CONF_KIOSK, default=True): cv.boolean,
+    cv.Optional(CONF_TOKEN): cv.string,
+    cv.Optional(CONF_STATS, default=True): cv.boolean,
     cv.Required(CONF_DISPLAY_ID): cv.use_id(cg.esphome_ns.class_("DisplayBuffer")),
     cv.Optional(CONF_TOUCHSCREEN_ID): cv.use_id(cg.esphome_ns.class_("Touchscreen")),
-    cv.Optional(CONF_KIOSK, default=True): cv.boolean,
-    cv.Optional(CONF_ROTATION, default=0): cv.one_of(0, 90, 180, 270, int=True),
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
@@ -27,8 +41,17 @@ async def to_code(config):
     await cg.register_component(var, config)
 
     cg.add(var.set_url(config[CONF_URL]))
+    cg.add(var.set_host(config.get(CONF_HOST, "")))
+    cg.add(var.set_port(config[CONF_PORT]))
+    cg.add(var.set_width(config[CONF_WIDTH]))
+    cg.add(var.set_height(config[CONF_HEIGHT]))
+    cg.add(var.set_rotate(config[CONF_ROTATE]))
+    cg.add(var.set_touch_rotate(config[CONF_TOUCH_ROTATE]))
     cg.add(var.set_kiosk(config[CONF_KIOSK]))
-    cg.add(var.set_rotation(config[CONF_ROTATION]))
+    cg.add(var.set_stats(config[CONF_STATS]))
+
+    if CONF_TOKEN in config:
+        cg.add(var.set_token(config[CONF_TOKEN]))
 
     display = await cg.get_variable(config[CONF_DISPLAY_ID])
     cg.add(var.set_display(display))
