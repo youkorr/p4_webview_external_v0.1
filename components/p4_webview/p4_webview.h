4b871432e9868718c1b5f0be63139042812cba4a
youkorr <96953496+youkorr@users.noreply.github.com>
2026-08-23 04:27:36 +0200

    p4_webview: add panel URL, dimensions and independent rotations

--- components/p4_webview/p4_webview.h
+++ components/p4_webview/p4_webview.h
@@ -13,8 +13,15 @@ namespace p4_webview {
 class P4WebView : public Component {
  public:
   void set_url(const std::string &url) { this->url_ = url; }
+  void set_host(const std::string &host) { this->host_ = host; }
+  void set_port(uint16_t port) { this->port_ = port; }
+  void set_width(uint16_t width) { this->width_ = width; }
+  void set_height(uint16_t height) { this->height_ = height; }
+  void set_rotate(uint16_t rotate) { this->rotate_ = rotate; }
+  void set_touch_rotate(uint16_t rotate) { this->touch_rotate_ = rotate; }
   void set_kiosk(bool kiosk) { this->kiosk_ = kiosk; }
-  void set_rotation(uint16_t rotation) { this->rotation_ = rotation; }
+  void set_token(const std::string &token) { this->token_ = token; }
+  void set_stats(bool stats) { this->stats_ = stats; }
 
   void set_display(display::DisplayBuffer *display) { this->display_ = display; }
 
@@ -29,8 +36,15 @@ class P4WebView : public Component {
 
  protected:
   std::string url_;
+  std::string host_;
+  std::string token_;
+  uint16_t port_{8123};
+  uint16_t width_{800};
+  uint16_t height_{1280};
+  uint16_t rotate_{0};
+  uint16_t touch_rotate_{0};
   bool kiosk_{true};
-  uint16_t rotation_{0};
+  bool stats_{true};
   display::DisplayBuffer *display_{nullptr};
 
 #ifdef USE_TOUCHSCREEN
